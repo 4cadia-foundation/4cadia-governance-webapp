fdb1d441dcc4a56abc13df6275ebf15868175dd6
Larissa Dias <larissa.carvalhod@outlook.com>
2019-10-09 15:42:59 -0300

    Customize block paragraph

--- src/components/Paragraph.vue
+++ src/components/Paragraph.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="paragraph">
-    <h2 class="title" v-if="paragraph.title" v-html="paragraph.title"></h2>
-    <div class="content">
-      <div class="text" v-if="paragraph.text" v-html="paragraph.text"></div>
+  <div :class="['paragraph', paragraph.containerClass]">
+    <div class="content-text">
+      <h2 class="title" v-if="paragraph.title" v-html="paragraph.title"></h2>
+      <p class="text" v-if="paragraph.text" v-html="paragraph.text"></p>
       <slot></slot>
     </div>
   </div>
@@ -23,15 +23,46 @@ export default {
   }
 }
 </script>
+
 <style scoped>
 .paragraph {
   position: relative;
   margin: auto;
   padding: 10px 30px;
   align-items: center;
-  /* text-align: left; */
+  float: none;
+  clear: both;
+  background-repeat: no-repeat;
+  display: flex;
+  background-size: contain;
 }
-.paragraph .text {
-  text-align: left;
+
+.paragraph.text-left {
+ /*float: left;*/
+ text-align: left;
+ justify-content: flex-start;
+ background-image: url("../assets/images/symbol-group-a.svg");
+ background-position: 115% center;
+
+}
+
+.paragraph .content-text {
+  max-width: 52%;
+}
+
+.paragraph.text-right {
+ /*float: right;*/
+ text-align: right;
+ justify-content: flex-end;
+ background-image: url("../assets/images/symbol-group-c.svg");
+ background-position: -8% center;
+}
+
+.paragraph.paragraph.text-left .content-text .title{
+  color:#071D49;
+}
+
+.paragraph.paragraph.text-right .content-text .title{
+  color:#0047BB;
 }
 </style>
